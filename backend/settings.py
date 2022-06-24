da49b20ac8bb00ebc2c75da9c4c7b52cd17e61db
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-06-24 17:08:11 +0000

    setting up .env

--- backend/settings.py
+++ backend/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+# setting django environment variable
+import environ
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b)@vg^kzgu60^)if)xii=0^03wjzvx68fg#e-mkzwj^91*+x_4'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
