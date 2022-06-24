2c771154274da27da790d90bd64165304b61b82d
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-06-24 17:19:35 +0000

    setting up api app

--- api/models.py
+++ api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Project(models.Model):
+    '''
+  Project models
+  '''
+    title = models.CharField(max_length=255, blank=True)
+    description = models.CharField(max_length=255, blank=True)
+    codeUrl = models.URLField()
+    liveDemoUrl = models.URLField()
+    previewImageUrl = models.URLField()
+    heroImageUrl = models.URLField()
+    body = models.CharField(max_length=255, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
