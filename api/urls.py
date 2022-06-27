1e2cba92f038b24fd6cfc44bcfe8109e98abab80
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-06-27 16:36:32 +0100

    installed jwt for authentication🔑

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 app_name = 'api'
 
@@ -10,4 +12,6 @@ urlpatterns = [
     path('project/<str:pk>/', views.project_detail, name='project-detail'),
     path('project/<str:pk>/update', views.project_update, name='project-update'),
     path('project/<str:pk>/delete', views.project_delete, name='project-delete'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- api/views.py
+++ api/views.py
@@ -4,9 +4,10 @@ from urllib3 import Retry
 # from rest_framework import serializers
 from .models import Project
 from .serializers import ProjectSerializer
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 # Create your views here.
 
@@ -24,6 +25,7 @@ def index(request):
 
 
 @api_view(['GET'])
+@permission_classes([IsAdminUser])
 def project_list(request):
     projects = Project.objects.all()
     serializer = ProjectSerializer(projects, many=True)
@@ -31,6 +33,7 @@ def project_list(request):
 
 
 @api_view(['POST'])
+@permission_classes((IsAuthenticated,))
 def project_add(request):
     serializer = ProjectSerializer(data=request.data)
 
